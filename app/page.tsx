f930571c02ef8e7c5288524d739938e5e6c7fe39
Sujata Gunale <67959015+TidbitsJS@users.noreply.github.com>
2023-11-12 11:25:38 +0530

    test tes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
 import Link from "next/link";
-import { getAllCourses, getSingleCourse } from "punn";
+import {
+  getAllCourses,
+  getSingleCourse,
+  getLectureContent,
+  generateCurriculumStructure,
+} from "punn";
 
 export default async function Home() {
-  const allCourses = await getSingleCourse("next-pro");
+  const allCourses = await getAllCourses();
+  const singleCourse = await getSingleCourse("next-pro");
+  const courseCurriculum = await generateCurriculumStructure("next-pro");
+  const lecture = await getLectureContent(
+    "next-pro",
+    "003_data-fetching",
+    "003_isr"
+  );
 
   return (
     <main className='flex min-h-screen flex-col p-24 max-w-5xl mx-auto'>
@@ -24,6 +36,15 @@ export default async function Home() {
           <hr />
 
           <pre>{JSON.stringify(allCourses, null, 2)}</pre>
+          <hr />
+
+          <pre>{JSON.stringify(singleCourse, null, 2)}</pre>
+          <hr />
+
+          <pre>{JSON.stringify(courseCurriculum, null, 2)}</pre>
+          <hr />
+
+          <pre>{JSON.stringify(lecture, null, 2)}</pre>
         </div>
       </section>
     </main>
